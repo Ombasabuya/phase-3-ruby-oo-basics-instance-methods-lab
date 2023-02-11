5dc55797438a2e0496014805dca2625f99956365
ombasaabuya <ombasabuya@gmail.com>
2023-02-11 06:27:18 +0300

    completed person and Dog

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+# class dog
+class Dog
+    #methods bark
+    def bark
+        puts  "Woof!"
+        def sit
+            puts "The Dog is sitting"
+            end
+    end 
+end
+fido = Dog.new
+fido.bark #=> "Woof!"
+
+snoopy = Dog.new
+snoopy.bark #=> "Woof!"
+
